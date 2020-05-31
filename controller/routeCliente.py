f1152fbe69c0234fac5dd6bb9c1d1aa80ad9d343
Miguekos <miguel.rodriguez@acceso.com.pe>
2020-05-31 01:49:41 -0500

    update v0.10.5 graficas

--- controller/routeCliente.py
+++ controller/routeCliente.py
@@ -197,8 +197,8 @@ def clientsReporte():
 
     resp = jsonify({
         "clientes": pcs + ps,
-        "clientesCS": pcs,
-        "clientesS": ps,
+        "clientesCS": int("{:.0f}".format(pcs * 100 / (pcs + ps))),
+        "clientesS": int("{:.0f}".format(ps * 100 / (pcs + ps))),
         # # "clientesC1": clientesC1,
         # # "clientesC2": clientesC2,
         # # "clientesC3": clientesC3,
@@ -208,6 +208,83 @@ def clientsReporte():
         "graficDate": FechaTodo()
     })
     return resp
+
+
+@app.route('/clientes/reporte/order')
+def clientsReporteOrder():
+    # user = mongo.db.clientes.find()
+    agr = [
+        {
+            '$group':
+                {
+                    '_id': {'dni': '$dni', 'enfermo': '$estados'}, 'personal': {'$push': "$$ROOT"},
+                    "estados": {"$push": "$estados"},
+                    "nombre": {"$last": "$nombre"},
+                    "ultimaFecha": {"$last": "$created_at"},
+                    "ulrimoEstado": {"$last": "$estados"},
+                    "count": {"$sum": 1}
+                }
+        }
+        # {'$addFields':
+        #     {
+        #         'estados': {'$addToSet': "$estados"}
+        #     }
+        # },
+    ]
+
+    # agr = [
+    #     {
+    #         '$group':
+    #             {
+    #                 '_id': '$dni', 'enformo': '$estados', 'personal': {'$push': "$$ROOT"},
+    #                 "estados": {"$push": "$estados"},
+    #                 "nombre": {"$last": "$nombre"},
+    #                 "ultimaFecha": {"$last": "$created_at"},
+    #                 "ulrimoEstado": {"$last": "$estados"},
+    #                 "count": {"$sum": 1}
+    #             }
+    #     }
+    #     # {'$addFields':
+    #     #     {
+    #     #         'estados': {'$addToSet': "$estados"}
+    #     #     }
+    #     # },
+    # ]
+
+    val = list(mongo.db.clientes.aggregate(agr))
+    # print(val)
+    return dumps(val)
+
+    # print(arg)
+    # print(asd)
+    # for post in asd:
+    #     # from_date.append(post['created_at'].strftime("%Y-%m-%d"))
+    #     # to_date.append(post['created_at'].strftime("%Y-%m-%d"))
+    #     # print(post['created_at'].strftime("%Y-%m-%d"))
+    #     # print(now.strftime("%Y-%m-%d"))
+    #     print(post)
+
+    # print(qwe)
+
+    # ps = mongo.db.clientes.count({"estados": "00"})
+    # pcs = mongo.db.clientes.count({"estados": "01"})
+    #
+    # countPCS = len(list(pcs))
+    # countPS = len(list(ps))
+
+    # resp = jsonify({
+    #     "clientes": pcs + ps,
+    #     "clientesCS": "{:.0f}".format(pcs * 100 / (pcs + ps)),
+    #     "clientesS": "{:.0f}".format(ps * 100 / (pcs + ps)),
+    #     # # "clientesC1": clientesC1,
+    #     # # "clientesC2": clientesC2,
+    #     # # "clientesC3": clientesC3,
+    #     # # "clientesC4": clientesC4,
+    #     "graficSeriaCS": qwe,
+    #     "graficSeriaS": asd,
+    #     "graficDate": FechaTodo()
+    # })
+    # return resp
 
 
 @app.route('/clientes')
