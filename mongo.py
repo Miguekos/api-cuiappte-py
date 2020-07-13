aab3d7c4ae5bca9aea23b3113bbe9ed4c8520a7e
Miguekos <miguekos1233@gmail.com>
2020-07-13 13:18:16 -0500

    add medifarma

--- mongo.py
+++ mongo.py
@@ -2,6 +2,6 @@ from app import app
 from flask_pymongo import PyMongo
 
 app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://95.111.235.214:32773/cuiappte"
+app.config["MONGO_URI"] = "mongodb://95.111.235.214:32773/cuiapptemedifarma"
 # app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/cuiappte"
 mongo = PyMongo(app)
